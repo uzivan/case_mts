5375bc0077c0c8995abcbefd00083730360d7b13
Ivan <ivan.olegovich603@gmail.com>
2024-03-14 15:58:41 +0300

    zero commit2

---
+++ src/main/java/com/mts/mts_case/MtsCaseApplication.java
@@ -0,0 +1,13 @@
+package com.mts.mts_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtsCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mts/mts_case/MtsCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.mts_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
